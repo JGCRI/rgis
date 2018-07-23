f223c78b0a90186427e5cc7717115691f65d931f
Vernon <d3y010@pe11690.pnl.gov>
2018-07-23 16:13:09 -0400

    adding import from statement for dplyr %>%

--- R/rgis.R
+++ R/rgis.R
@@ -10,31 +10,32 @@
 #'
 #' \strong{Analysis Tools:}
 #'
-#' polygon_bounding_box - Create a spatial polygon bounding box sf object from an extent and CRS
-#' buld_fishnet - Build a vector polygon fishnet from reference object bounds and user-defined resolution
+#' \code{polygon_bounding_box} - Create a spatial polygon bounding box sf object from an extent and CRS
+#' \code{buld_fishnet} - Build a vector polygon fishnet from reference object bounds and user-defined resolution
 #'
 #' \strong{Conversion Tools:}
 #'
-#' polygon_to_raster - Create a raster from a polygon sf object
-#' raster_to_polygon - Create a polygon sf object from a raster
-#' ncdf_to_csv - Convert NetCDF file to a CSV
+#' \code{polygon_to_raster} - Create a raster from a polygon sf object
+#' \code{raster_to_polygon} - Create a polygon sf object from a raster
+#' \code{ncdf_to_csv} - Convert NetCDF file to a CSV
 #'
 #' \strong{Input/Output (IO) Tools:}
 #'
-#' import_shapefile - Create an sf object from a shapefile
-#' import_raster - Create a raster object from a file path
-#' import_ncdf_to_raster - Import NetCDF to brick raster
-#' import_points_from_csv - Import point data from a CSV file
+#' \code{import_shapefile} - Create an sf object from a shapefile
+#' \code{import_raster} - Create a raster object from a file path
+#' \code{import_ncdf_to_raster} - Import NetCDF to brick raster
+#' \code{import_points_from_csv} - Import point data from a CSV file
 #'
 #'
 #' \strong{Modification Tools:}
 #'
-#' add_area_field - Add and calculate an area field for polygon sf objects
+#' \code{add_area_field} - Add and calculate an area field for polygon sf objects
 #'
 #' \strong{Workflow Tools:}
 #'
-#' grid_to_zone_fraction - Create fractional area of NetCDF grid cells to corresponding polygon features and vice versa
+#' \code{grid_to_zone_fraction} - Create fractional area of NetCDF grid cells to corresponding polygon features and vice versa
 #'
 #' @docType package
 #' @name rgis
+#' @importFrom dplyr %>%
 NULL
