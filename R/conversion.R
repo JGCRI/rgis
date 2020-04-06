383fb7705de983f7aca8978cf1421f441c4bc55a
Kanishka Narayan <37234086+kanishkan91@users.noreply.github.com>
2020-04-06 15:04:03 -0400

    Adding a warp_raster function which uses gdalwarp.
    1. If the dataset has no_data, assigns a value of 0 to all no_data, takes off no_data tag.
    2. Reprojects to the given dimensions.
    3. User can specify output type, aggregation method, co-ordinate system etc.

--- R/conversion.R
+++ R/conversion.R
@@ -95,3 +95,53 @@ ncdf_to_csv <- function(ncdf_file, out_csv, nc_var_name, resolution = 0.5, start
   
   return(combined)
 }
+
+#' warp_raster
+#'
+#' Convert raster to specific dimensions and formats
+#'
+#' @param inupt_file location of input file
+#' @param input_format format of the input file  
+#' @param NO_DATA_STATUS Boolean for no_data values. Set this to true if there are NO_DATA values in the data set. If there are no data values, this will fill them with 0
+#' @param no_data_value Value of no_data in the dataset
+#' @param method Method of conversion. Default is set to average 
+#' @param projection Output projection type. Defualt is set to +proj=longlat +ellps=WGS84
+#' @param col_row columns and rows in output dataset. Defualt is set to c(4320,2160) 
+#' @param extent output extent. Default is set to c(-180,-90,180,90)
+#' @param output_format Format of output. Currently set to ENVI.
+#' @param out_file Name of output file
+#' @importFrom gdalUtilities gdalwarp gdal_translate
+#' @author Kanishka Narayan (kanishka.narayan@pnnl.gov)
+#' @export
+
+warp_raster<- function(input_file='input.tif',input_format=".tif",NO_DATA_STATUS=TRUE,method="average", no_data_value=127,
+                       resolution="+proj=longlat +ellps=WGS84",col_row=c(4320,2160),
+                       extent=c(-180,-90,180,90),out_file="outfile",output_format="ENVI"){
+  
+  
+  if (NO_DATA_STATUS==TRUE){
+    print("Warning: filling no_data values may take a significantly long time  with big datasets")
+    
+    gdalwarp(srcfile = input_file, dstfile = paste0("tmp",input_format) ,srcnodata = no_data_value,dstnodata = 0)
+    
+    gdal_translate(a_nodata = 0, src_dataset = paste0("tmp",input_format),dst_dataset = paste0("tmp1",input_format))
+    
+    file.remove(paste0("tmp",input_format))
+    
+    #Take off no-data tag
+    gdal_translate(a_nodata = "none", src_dataset = paste0("tmp1",input_format),dst_dataset = paste0("tmp2",input_format))
+    
+    file.remove(paste0("tmp1",input_format))
+    
+    return(gdalwarp(paste0("tmp2",input_format),out_file,r= method,
+             ot="Float32",t_srs=resolution, te= extent,ts=col_row, of=output_format))
+    
+    file.remove(paste0("tmp2",input_format))
+  }else{
+  
+  return(gdalwarp(input_file,out_file,r= method,
+           ot="Float32",t_srs=resolution, te= extent,ts=col_row, of=output_format))
+  
+}
+}
+
