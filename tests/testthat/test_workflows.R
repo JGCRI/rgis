197c9954fb69140bc66c7db6b265a6b36de8017c
Kanishka Narayan <37234086+kanishkan91@users.noreply.github.com>
2020-11-02 21:17:54 -0500

    Adding tests for intersections, zone fractions functions

---
+++ tests/testthat/test_workflows.R
@@ -0,0 +1,30 @@
+context("test workflow functions")
+library(raster)
+
+test_that("Test intersection fraction computation", {
+  expect_warning(test_output <- get_intersection_fractions(out_csv = NULL,out_shape_file = NULL,shpfile_1 = "test_data/shp/tl_2019_us_state.shp",
+                                            shpfile_2 = "test_data/shp/glu_boundaries_moirai_combined_3p1_0p5arcmin.shp"))
+  
+  tmpna <- test_output[is.na(test_output$ratio_1),]
+  tmpna2 <- test_output[is.na(test_output$ratio_2),]
+  
+  expect(nrow(tmpna)==0 & nrow(tmpna2)==0,"NA values being returned by intersection fractions function")
+})
+
+test_that("Test zone fractions computation", {
+  #Import the raster
+  expect_warning(test_output <- grid_to_zone_fraction(poly_path = "test_data/shp/northcarolina_county_wgs84.shp",
+                                                      ncdf_path = "test_data/nc/tas_watch_monthly_1991.nc4",out_csv = "tmp.csv"))
+  test_output %>% 
+    group_by(GEOID) %>% 
+    mutate(zone_frac=sum(as.double(zone_frac)),cell_frac=sum(as.double(zone_frac))) %>% 
+    ungroup() %>% 
+    select(GEOID,zone_frac) %>% 
+    distinct()->grouped_output
+  
+  tmp <- grouped_output %>% filter(zone_frac < 0.96)
+  
+  expect(nrow(tmp)==0 ,"Zone fractions not adding up to 1")
+  
+})
+
